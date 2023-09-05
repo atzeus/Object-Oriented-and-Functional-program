4d3059e973c2d9a9a6565e3829a3e7ed6e6f42fe
Atze <atzeus@gmail.com>
2023-09-05 11:02:58 +0200

    remove

--- gamebase-snake/src/snake/logic/Point.scala
+++
@@ -1,5 +0,0 @@
-package snake.logic
-
-// you can alter this file!
-
-case class Point(x : Int, y : Int)
--- gamebase-sokoban/src/sokoban/logic/Point.scala
+++
@@ -1,5 +0,0 @@
-package sokoban.logic
-
-case class Point(x : Int, y : Int) {
-  def +(rhs : Point) : Point = Point(x + rhs.x, y + rhs.y)
-}
